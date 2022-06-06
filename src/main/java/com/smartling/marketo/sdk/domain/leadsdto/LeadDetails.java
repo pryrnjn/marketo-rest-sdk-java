1246a592c276db6328bb450b82e7b6f91314d9c9
sushil.mall <sushil.mall@ringcentral.com>
2022-06-06 13:03:48 +0530

    fetch leadsData by filterType and filterValuess

---
+++ src/main/java/com/smartling/marketo/sdk/domain/leadsdto/LeadDetails.java
@@ -0,0 +1,13 @@
+package com.smartling.marketo.sdk.domain.leadsdto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter@Setter
+public class LeadDetails{
+    private Integer id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String postalCode;
+}
--- src/main/java/com/smartling/marketo/sdk/rest/MarketoLeadClient.java
+++ src/main/java/com/smartling/marketo/sdk/rest/MarketoLeadClient.java
@@ -3,7 +3,9 @@ package com.smartling.marketo.sdk.rest;
 import com.smartling.marketo.sdk.MarketoApiException;
 import com.smartling.marketo.sdk.domain.leadsdto.CreateLeadsRequestDto;
 import com.smartling.marketo.sdk.domain.leadsdto.CreateLeadsResponseDto;
+import com.smartling.marketo.sdk.domain.leadsdto.LeadInfoDto;
 import com.smartling.marketo.sdk.rest.command.lead.CreateLeads;
+import com.smartling.marketo.sdk.rest.command.lead.FilterLeadsData;
 
 import java.util.List;
 
@@ -14,7 +16,18 @@ public class MarketoLeadClient {
     public MarketoLeadClient(HttpCommandExecutor httpCommandExecutor) {
         this.httpCommandExecutor = httpCommandExecutor;
     }
-    public List<CreateLeadsResponseDto> requestCreateLeads(CreateLeadsRequestDto createLeadsRequestDto) throws MarketoApiException {
-        return httpCommandExecutor.execute(new CreateLeads(createLeadsRequestDto));
+    public CreateLeadsResponseDto requestCreateLeads(CreateLeadsRequestDto createLeadsRequestDto) throws MarketoApiException {
+        List<CreateLeadsResponseDto> createLeadsResponseDtoList=httpCommandExecutor.execute(new CreateLeads(createLeadsRequestDto));
+        if(createLeadsResponseDtoList.isEmpty())
+            return null;
+        return createLeadsResponseDtoList.get(0);
     }
+
+    public LeadInfoDto findLeadsByFilterType(String filterType,String filterValues) throws MarketoApiException {
+       List<LeadInfoDto> leadInfoDtos= httpCommandExecutor.execute(new FilterLeadsData(filterType,filterValues));
+       if(leadInfoDtos.isEmpty())
+           return null;
+       return leadInfoDtos.get(0);
+    }
+
 }
