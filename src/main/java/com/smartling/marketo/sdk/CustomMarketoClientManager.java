96918558fa9627718ac1b0f69b32a3d74e36bbe8
sushil.mall <sushil.mall@ringcentral.com>
2022-08-02 18:21:46 +0530

    removed files and add in MWS

--- src/main/java/com/smartling/marketo/sdk/CustomMarketoClientManager.java
+++
@@ -1,7 +0,0 @@
-package com.smartling.marketo.sdk;
-
-import com.smartling.marketo.sdk.rest.MarketoProgramMemberDataClient;
-
-public interface CustomMarketoClientManager {
-    MarketoProgramMemberDataClient getMarketoProgramMemberDataClient();
-}
